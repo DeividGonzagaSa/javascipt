893a47c67c4b3497b89e596c8e60c8fc28c64ece
DeividGonzagaSa <deividgonzaga31@gmail.com>
2024-05-06 18:23:00 -0300

    ex

---
+++ EXERCICIOS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if ( vel > 60) { 
+    console.log(`você utrapassou a velocidade permitida. MUTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança!`)
